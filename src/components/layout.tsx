ce070cca057bd7fd9fe3e1bc43e6e04bcc109f8a
amandeep674 <as674723@gmail.com>
2025-06-06 20:59:52 +0530

    updated code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: PropsWithChildren) {
       </main>
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
         <div className="container mx-auto px-4 text-center text-gray-200">
-          <p>Made with 💗 by RoadsideCoder</p>
+          <p>©️ 2025 All rights reserved</p>
         </div>
       </footer>
     </div>
